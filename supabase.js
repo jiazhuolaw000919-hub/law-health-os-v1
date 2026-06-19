0b60ca2c84f4a651e5b105907ac66b34e2b86ff0
jiazhuolaw000919-hub <jiazhuolaw000919@gmail.com>
2026-06-19 16:43:54 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,16 +2,14 @@ const SUPABASE_URL = "https://jwcgamxkwzrjnepxrvzr.supabase.co"
 const SUPABASE_KEY = "YOUR_ANON_KEY"
 
 async function getFood(date){
-
 const res = await fetch(
-SUPABASE_URL+"/rest/v1/food_logs?date=eq."+date,
+SUPABASE_URL + "/rest/v1/food_logs?date=eq." + date,
 {
 headers:{
 apikey:SUPABASE_KEY,
-Authorization:"Bearer "+SUPABASE_KEY
+Authorization:"Bearer " + SUPABASE_KEY
 }
 }
 )
-
 return res.json()
 }
