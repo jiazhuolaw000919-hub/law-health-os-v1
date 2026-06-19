1ca5869f332332de78f9314b200d8d36f4e21fa7
jiazhuolaw000919-hub <jiazhuolaw000919@gmail.com>
2026-06-19 16:26:20 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,24 +2,20 @@ const SUPABASE_URL = "https://jwcgamxkwzrjnepxrvzr.supabase.co"
 const SUPABASE_KEY = "PASTE_YOUR_ANON_KEY"
 
 async function insertFood(data){
-
-return await fetch(SUPABASE_URL + "/rest/v1/food_logs",{
+return fetch(SUPABASE_URL+"/rest/v1/food_logs",{
 method:"POST",
 headers:{
 "Content-Type":"application/json",
 apikey:SUPABASE_KEY,
-Authorization:"Bearer "+SUPABASE_KEY,
-Prefer:"return=minimal"
+Authorization:"Bearer "+SUPABASE_KEY
 },
 body:JSON.stringify(data)
 })
-
 }
 
-async function getFoodsByDate(date){
-
+async function getFoodByDate(date){
 const res = await fetch(
-SUPABASE_URL + "/rest/v1/food_logs?date=eq."+date,
+SUPABASE_URL+"/rest/v1/food_logs?date=eq."+date,
 {
 headers:{
 apikey:SUPABASE_KEY,
@@ -27,14 +23,12 @@ Authorization:"Bearer "+SUPABASE_KEY
 }
 }
 )
-
-return await res.json()
+return res.json()
 }
 
 async function getExerciseByDate(date){
-
 const res = await fetch(
-SUPABASE_URL + "/rest/v1/exercise_logs?date=eq."+date,
+SUPABASE_URL+"/rest/v1/exercise_logs?date=eq."+date,
 {
 headers:{
 apikey:SUPABASE_KEY,
@@ -42,6 +36,5 @@ Authorization:"Bearer "+SUPABASE_KEY
 }
 }
 )
-
-return await res.json()
+return res.json()
 }
