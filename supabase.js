9b0c042cb3a4d456b40620d3ba9e3759b28bf79a
jiazhuolaw000919-hub <jiazhuolaw000919@gmail.com>
2026-06-20 09:58:43 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,7 +3,8 @@ const SUPABASE_KEY = "YOUR_ANON_KEY"
 
 async function getFoodLogs(date){
 
-try{
+return safeAsync(async ()=>{
+
 const res = await fetch(
 `${SUPABASE_URL}/rest/v1/food_logs?date=eq.${date}`,
 {
@@ -18,37 +19,5 @@ if(!res.ok) return []
 
 return await res.json()
 
-}catch(e){
-return []
-}
-}
-
-async function getDailySummary(date){
-
-try{
-const res = await fetch(
-`${SUPABASE_URL}/rest/v1/food_logs?date=eq.${date}`,
-{
-headers:{
-apikey:SUPABASE_KEY,
-Authorization:"Bearer " + SUPABASE_KEY
-}
-}
-)
-
-const data = await res.json()
-
-let calories = 0
-
-data.forEach(item=>{
-calories += Number(item.calories || 0)
-})
-
-return {
-calories
-}
-
-}catch(e){
-return {calories:0}
-}
+},[])
 }
