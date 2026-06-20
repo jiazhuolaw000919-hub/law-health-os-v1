c28f8d26d7276aa7d8d79fe27d532477e15be8c6
jiazhuolaw000919-hub <jiazhuolaw000919@gmail.com>
2026-06-20 08:27:14 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = "https://jwcgamxkwzrjnepxrvzr.supabase.co"
 const SUPABASE_KEY = "YOUR_ANON_KEY"
 
 async function getFood(date){
-try{
+
 const res = await fetch(
 SUPABASE_URL + "/rest/v1/food_logs?date=eq." + date,
 {
@@ -15,19 +15,15 @@ Authorization:"Bearer " + SUPABASE_KEY
 
 if(!res.ok) return []
 return await res.json()
-
-}catch(e){
-console.log(e)
-return []
-}
 }
 
 async function addFood(data){
+
 return fetch(SUPABASE_URL + "/rest/v1/food_logs",{
 method:"POST",
 headers:{
 apikey:SUPABASE_KEY,
-Authorization:"Bearer "+SUPABASE_KEY,
+Authorization:"Bearer " + SUPABASE_KEY,
 "Content-Type":"application/json"
 },
 body:JSON.stringify(data)
