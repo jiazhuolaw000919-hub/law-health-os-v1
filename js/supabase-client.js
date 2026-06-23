c534d067c7bb709546154047ebef4b73638befe4
jiazhuolaw000919-hub <jiazhuolaw000919@gmail.com>
2026-06-23 11:46:53 +0800

    Create supabase-client.js

---
+++ js/supabase-client.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
+
+export const supabase = createClient(
+  "https://jqevcfyhnlttzdiylfrh.supabase.co",
+  "sb_publishable_GMM5PgeRzudbeh4aHK-1pw_lQ6TQnVe"
+)
